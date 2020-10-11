9df4da7b6428f3ff1c94f7c882fe7ab758aa8a3b
enzodn <enzodn@protonmail.com>
2020-10-11 22:11:20 +0200

    first commit

---
+++ config.py
@@ -0,0 +1,6 @@
+
+# Old directory path
+INPUT_PATH = 'OLD_PDF'
+
+# New directory path
+OUTPUT_PATH = 'NEW_PDF'
---
+++ main.py
@@ -0,0 +1,47 @@
+import os
+import PyPDF2
+from config import *
+
+
+def clean_pdf(pdf_input):
+    pdf_reader = PyPDF2.PdfFileReader(f'{INPUT_PATH}/{pdf_input}')
+    pdf_writer = PyPDF2.PdfFileWriter()
+    last_page_n = pdf_reader.getNumPages()-1
+
+    for i in range(last_page_n):
+        current_page = pdf_reader.getPage(i)
+        next_page = pdf_reader.getPage(i + 1)
+        print(f'Checked page: {i}')
+        if current_page.extractText()[-6] != next_page.extractText()[-6]:
+            pdf_writer.addPage(current_page)
+            print(f'Added page: {i}')
+    # Add last page
+    if pdf_reader.getPage(last_page_n-1) != pdf_reader.getPage(last_page_n):
+        pdf_writer.addPage(pdf_reader.getPage(last_page_n))
+        print(f'Added page: {last_page_n}')
+
+    pdf_output = f'{OUTPUT_PATH}/{pdf_input}'
+
+    with open(pdf_output, 'wb') as f:
+        pdf_writer.write(f)
+
+
+def make_directory():
+    try:
+        os.mkdir(OUTPUT_PATH)
+        print("Directory created ")
+    except FileExistsError:
+        print("Directory already exists")
+
+
+def clean_pdf_all():
+    make_directory()
+    for entry in os.scandir(INPUT_PATH):
+        print(f'Processing file: {entry.name}')
+        clean_pdf(entry.name)
+
+
+if __name__ == '__main__':
+    clean_pdf_all()
+
+
