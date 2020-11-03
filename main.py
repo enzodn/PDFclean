b224fc2630a453b359d22225d292136e45a02314
enzodn <enzodn@protonmail.com>
2020-11-03 07:50:14 +0100

    Small improvements

--- main.py
+++ main.py
@@ -3,21 +3,29 @@ import PyPDF2
 from config import *
 
 
+def get_number_before_separator(page):
+    return page.extractText().rsplit(SEPARATOR, 1)[0][-1]
+
+
 def clean_pdf(pdf_input):
     pdf_reader = PyPDF2.PdfFileReader(f'{INPUT_PATH}/{pdf_input}')
     pdf_writer = PyPDF2.PdfFileWriter()
-    last_page_n = pdf_reader.getNumPages()-1
+    last_page_n = pdf_reader.getNumPages() - 1
 
     for i in range(last_page_n):
         current_page = pdf_reader.getPage(i)
         next_page = pdf_reader.getPage(i + 1)
         print(f'Checked page: {i}')
-        if current_page.extractText()[-6] != next_page.extractText()[-6]:
+        if get_number_before_separator(current_page) != get_number_before_separator(next_page):
             pdf_writer.addPage(current_page)
             print(f'Added page: {i}')
+
     # Add last page
-    if pdf_reader.getPage(last_page_n-1) != pdf_reader.getPage(last_page_n):
-        pdf_writer.addPage(pdf_reader.getPage(last_page_n))
+    new_pdf_page_numbers = pdf_writer.getNumPages() - 1
+    new_pdf_last_page = pdf_writer.getPage(new_pdf_page_numbers)
+    old_pdf_last_page = pdf_reader.getPage(last_page_n)
+    if old_pdf_last_page != new_pdf_last_page:
+        pdf_writer.addPage(old_pdf_last_page)
         print(f'Added page: {last_page_n}')
 
     pdf_output = f'{OUTPUT_PATH}/{pdf_input}'
@@ -29,7 +37,7 @@ def clean_pdf(pdf_input):
 def make_directory():
     try:
         os.mkdir(OUTPUT_PATH)
-        print("Directory created ")
+        print("Directory created")
     except FileExistsError:
         print("Directory already exists")
 
@@ -43,5 +51,3 @@ def clean_pdf_all():
 
 if __name__ == '__main__':
     clean_pdf_all()
-
-
